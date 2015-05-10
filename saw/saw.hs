b85a7ee252e1423ef0945daa9af576b743530723
iambernie <iambernie@gmail.com>
2015-05-10 12:53:23 +0200

    removed unused functions

--- saw/saw.hs
+++ saw/saw.hs
@@ -8,23 +8,10 @@ data Saw = Saw {configurations :: String}
 
 sawArgs = Saw {configurations = def}
 
-data PolyTree a = End
-                | Branch a (PolyTree a) (PolyTree a) (PolyTree a) (PolyTree a)
-                deriving (Show)
-
-
 ptree :: (Show a) => Tree a -> IO ()
 ptree = putStrLn . drawTree . stringTree
       where  stringTree :: (Show a) => Tree a -> Tree String
              stringTree = fmap show
-
-listpaths :: Tree a -> [[a]]
-listpaths (Node label []) = [[label]]
-listpaths (Node label xs) = map (label:) $ concat $ map listpaths xs
-
-listendpoints :: Tree a -> [a]
-listendpoints (Node label []) = [label]
-listendpoints (Node label trees) = concat $ map listendpoints trees
 
 type Punt = (Int, Int)
 type Path = [Punt]
@@ -40,13 +27,8 @@ move East  (x,y) = (x+1, y)
 moves :: [Punt -> Punt]
 moves = [move North, move South, move West, move East]
 
-origin = (0,0) :: Punt
-
 root :: Tree (Punt, Path)
 root = Node ((0,1), [(0,0)]) []
-
-sqrdDistance :: Punt -> Int
-sqrdDistance (x,y) = x^2+y^2
 
 allowedMoves :: Path -> Punt -> [Punt]
 allowedMoves path pt =  filter (`notElem` path) [move pt | move <- moves]
@@ -60,14 +42,11 @@ nTree :: Int -> Tree (Punt, Path)
 nTree 0 = root
 nTree n = iterate insertMoves (nTree 0) !! (n-1)
 
--- evt trees = iterate insertMoves (nTree 0)
-
-fpow n = foldr (.) id . replicate n
+--fpow n = foldr (.) id . replicate n
 
 paths :: [(Punt, Path)] -> [Path]
 paths ((punt, path):[])  = [path]
 paths ((punt, path):xs)  = path:[] ++ paths xs
-
 
 pathlengths :: Tree (Punt, Path) -> [Int]
 pathlengths tree = map length $ (paths . flatten) tree
@@ -84,11 +63,6 @@ isLength n (punt, path) = length path == n
 reSqrd :: Int -> Float
 reSqrd n = fromIntegral (sumSqrdDistances n) / fromIntegral (zsaw n)
 
-
 main =  cmdArgs sawArgs >>= print
-     -- run program
-
-     -- writetofile
      --print (map zsaw [1..10]) >>
-     --print "done"
 
